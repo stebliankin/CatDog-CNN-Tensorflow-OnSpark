db9dd52c0bb394040973a8af573dd83567cb433b
Vitalii <steblyankin@gmail.com>
2018-11-11 22:29:46 -0500

    safe mkdir

--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
+import os
+
 def write_log(date, message, logfile):
     with open(logfile, 'a') as log:
         log.write(date+" "+message+"\n")
+def safe_mkdir(path):
+    """ Create a directory if there isn't one already. """
+    try:
+        os.mkdir(path)
+    except OSError:
+        pass
