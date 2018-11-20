a294a483e11ef7ded389aa284b3600cc4df6ed63
Vitalii <steblyankin@gmail.com>
2018-11-20 11:48:01 -0500

    adding communication between worker and parameter server

--- local_clusters/conv_net_spark.py
+++ local_clusters/conv_net_spark.py
@@ -5,6 +5,8 @@ from tensorflowonspark import TFCluster, TFNode
 
 import sys
 
+
+
 sys.path.append("..")
 
 import conv_net
@@ -23,25 +25,39 @@ def main_fun(argv, ctx):
                                """Directory with checkpoints """)
     tf.app.flags.DEFINE_string('graph_path', '../../graphs/catdog_spark',
                                """Directory with graphs """)
-    tf.app.flags.DEFINE_integer('dataset_size', 25000,
+    tf.app.flags.DEFINE_integer('dataset_size', 200,
                                """Dataset size """)
-    tf.app.flags.DEFINE_integer('batch_size', 100,
+    tf.app.flags.DEFINE_integer('batch_size', 32,
                                 """Batch Size """)
-    tf.app.flags.DEFINE_integer('n_epoch', 10,
+    tf.app.flags.DEFINE_integer('n_epoch', 2,
                                 """Number Of Epoch """)
+    tf.app.flags.DEFINE_boolean('rdma', False, """Whether to use rdma.""")
 
     FLAGS=tf.app.flags.FLAGS
 
-    model = conv_net.CatDogConvNet(FLAGS.checkpoint_path, FLAGS.graph_path, dataset_size=FLAGS.dataset_size, batch_size=FLAGS.batch_size)
-    model.training_folder = FLAGS.train_dir
-    print('building a model')
-    model.build()
-    print('training')
-    model.train(n_epochs=FLAGS.n_epoch)
+    cluster, server = ctx.start_cluster_server(1, FLAGS.rdma)
+
+    worker_num = ctx.worker_num
+    job_name = ctx.job_name
+    task_index = ctx.task_index
+    if job_name == "ps":
+        server.join()
+
+    elif job_name == "worker":
+        with tf.device(tf.train.replica_device_setter(
+                worker_device="/job:worker/task:%d" % task_index,
+                cluster=cluster)):
+
+            model = conv_net.CatDogConvNet(FLAGS.checkpoint_path, FLAGS.graph_path, dataset_size=FLAGS.dataset_size, batch_size=FLAGS.batch_size)
+            model.training_folder = FLAGS.train_dir
+            print('building a model')
+            model.build()
+            print('training')
+            model.train(n_epochs=FLAGS.n_epoch)
 
 if __name__ == '__main__':
   sc = SparkContext(conf=SparkConf().setAppName("catdog_spark"))
-  num_executors = 2
+  num_executors = 3
   num_ps = 1
 
   cluster = TFCluster.run(sc, main_fun, sys.argv, num_executors, num_ps, False, TFCluster.InputMode.TENSORFLOW)
