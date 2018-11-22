6925e77f858272c9e010029b8f27b361763d808f
Vitalii <steblyankin@gmail.com>
2018-11-21 21:14:22 -0500

    Adding tensorflow

--- local_clusters/conv_net_spark.py
+++ local_clusters/conv_net_spark.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from tensorflowonspark import TFCluster, TFNode
 
 import sys
-
+import argparse
 
 
 sys.path.append("..")
@@ -14,16 +14,25 @@ import conv_net
 def main_fun(argv, ctx):
     # argv - parameters from sys.argv
     # ctx - node metadata like job_name, task_id
+
+    main_path="/Users/stebliankin/Desktop/DataScience-CAP5768/project/"
+
+    sys.path.append(main_path+"CatDog-CNN-Tensorflow-OnSpark/")
+
     import tensorflow as tf
     import tensorflowonspark
     import conv_net
+    import utils
+    import datetime
 
     sys.argv = argv
-    tf.app.flags.DEFINE_string('train_dir', '../../data_catsdogs/train',
+
+
+    tf.app.flags.DEFINE_string('train_dir', main_path+'/data_catsdogs/train',
                                """Directory with training images """)
-    tf.app.flags.DEFINE_string('checkpoint_path', '../../checkpoints/catdog_spark',
+    tf.app.flags.DEFINE_string('checkpoint_path', main_path+'checkpoints/catdog_spark',
                                """Directory with checkpoints """)
-    tf.app.flags.DEFINE_string('graph_path', '../../graphs/catdog_spark',
+    tf.app.flags.DEFINE_string('graph_path', main_path+'graphs/catdog_spark',
                                """Directory with graphs """)
     tf.app.flags.DEFINE_integer('dataset_size', 200,
                                """Dataset size """)
@@ -40,28 +49,40 @@ def main_fun(argv, ctx):
     worker_num = ctx.worker_num
     job_name = ctx.job_name
     task_index = ctx.task_index
+    log_file=main_path+"log_spark.txt"
+
+    print(job_name)
     if job_name == "ps":
         server.join()
+        utils.write_log(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
+                        'PS: Merging the model',
+                        log_file)
 
     elif job_name == "worker":
-        with tf.device(tf.train.replica_device_setter(
-                worker_device="/job:worker/task:%d" % task_index,
-                cluster=cluster)):
-
-            model = conv_net.CatDogConvNet(FLAGS.checkpoint_path, FLAGS.graph_path, dataset_size=FLAGS.dataset_size, batch_size=FLAGS.batch_size)
-            model.training_folder = FLAGS.train_dir
-            print('building a model')
-            model.build()
-            print('training')
-            model.train(n_epochs=FLAGS.n_epoch)
+        model = conv_net.CatDogConvNet(FLAGS.checkpoint_path, FLAGS.graph_path, dataset_size=FLAGS.dataset_size,
+                                       batch_size=FLAGS.batch_size, num_workers=num_executors,
+                                       task_index=task_index, ctx=ctx)
+        model.training_folder = FLAGS.train_dir
+        model.log_file=main_path+"log_spark.txt"
+        print('building a model')
+        utils.write_log(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
+                        'building the model on worker {}; task index {}'.format(worker_num, task_index),
+                        log_file)
+        model.build()
+        print('training')
+        model.train(n_epochs=FLAGS.n_epoch)
 
 if __name__ == '__main__':
-  sc = SparkContext(conf=SparkConf().setAppName("catdog_spark"))
-  num_executors = 3
-  num_ps = 1
+    parser = argparse.ArgumentParser()
 
-  cluster = TFCluster.run(sc, main_fun, sys.argv, num_executors, num_ps, False, TFCluster.InputMode.TENSORFLOW)
-  cluster.shutdown()
+    parser.add_argument("--num_workers", required=True, type=str, help="Number of worker executing excluding parameter server")
+
+    sc = SparkContext(conf=SparkConf().setAppName("catdog_spark"))
+    num_executors = 3
+    num_ps = 1
+
+    cluster = TFCluster.run(sc, main_fun, sys.argv, num_executors, num_ps, False, TFCluster.InputMode.TENSORFLOW)
+    cluster.shutdown()
 
 
 
