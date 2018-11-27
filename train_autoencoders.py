898555bec6c4f423eae4ceb573b5b19a125a7bce
Vitalii <steblyankin@gmail.com>
2018-11-27 13:51:39 -0500

    Adding autoencoders

---
+++ train_autoencoders.py
@@ -0,0 +1,26 @@
+import conv_net
+import utils
+import time
+import datetime
+
+checkpoint_path = "../checkpoints/catdog"
+graph_path = "../graphs/cat_dog"
+log_file = "autoencoders_log.txt"
+dataset_size = 1000
+batch_size = 100
+n_epoch=10
+
+start = time.time()
+print('start program')
+utils.write_log(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"), "Starting run_catdog_conv.py", log_file)
+utils.write_log(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
+                "Dataset size: {}; Batch size {}".format(dataset_size, batch_size),
+                log_file)
+
+model = conv_net.CatDogConvNet(checkpoint_path, graph_path, dataset_size=dataset_size, batch_size=batch_size,
+                               log_file=log_file, n_channels=1)
+model.desired_shape = 256
+print('building a model')
+model.build_autoencoders()
+print('training')
+model.train_autoencoder(n_epoch)
